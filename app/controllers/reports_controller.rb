79862e1b162597b77db3e96640785c4661715b11
udaikue <ikueikue@gmail.com>
2021-01-21 11:46:57 +0900

    paramsの取得値を修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -59,6 +59,6 @@ class ReportsController < ApplicationController
   end
 
   def report_params
-    params.require(:report).permit(:title, :content, :user_id)
+    params.require(:report).permit(:title, :content)
   end
 end
